8c4e13be4c24315e019b0ec6070783c4f93ef3d5
Saundra <saundracatalina@gmail.com>
2020-12-02 15:28:15 -0700

    Add Candies table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_221905) do
+ActiveRecord::Schema.define(version: 2020_12_02_222548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "candies", force: :cascade do |t|
+    t.string "name"
+    t.integer "quantity"
+    t.boolean "in_stock"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "candy_shops", force: :cascade do |t|
     t.string "name"
