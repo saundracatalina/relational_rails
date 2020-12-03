89442287e47b2f2e6aca75b16e56686047a0c8b4
Megan Campbell <megan.campbell26@yahoo.com>
2020-12-02 18:38:43 -0700

    Add user story 2

--- app/controllers/concerns/distribution_centers_controller.rb
+++ app/controllers/concerns/distribution_centers_controller.rb
@@ -2,4 +2,8 @@ class DistributionCentersController < ApplicationController
   def index
     @distribution_centers = DistributionCenter.all
   end
+
+  def show
+    @distribution_center = DistributionCenter.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/distribution_centers', to: 'distribution_centers#index'
+  get '/distribution_centers/:id', to: 'distribution_centers#show'
 end
