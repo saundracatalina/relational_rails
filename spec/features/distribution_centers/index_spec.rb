8c90429e575913b3885de753e83be3f7410370c9
Megan Campbell <megan.campbell26@yahoo.com>
2020-12-04 10:59:54 -0700

    Add test for new dc link

--- spec/features/distribution_centers/index_spec.rb
+++ spec/features/distribution_centers/index_spec.rb
@@ -6,9 +6,15 @@ RSpec.describe "distribution centers index page" do
     dc_2 = DistributionCenter.create!(name: "Ships Ahoy", trucks_deployed: 25)
 
     visit "/distribution_centers"
-    
+
     expect(page).to have_content(dc_1.name)
     expect(page).to have_content(dc_2.name)
+  end
+  it "has link to create a new distribution center record" do
+
+  visit "/distribution_centers"
+
+  expect(page).to have_link("New Distribution Center", href: '/distribution_centers/new')
   end
 end
 
