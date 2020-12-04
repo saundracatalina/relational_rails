f2abf1f44605bcba7406dcd8e6509228e0e9d347
Saundra <saundracatalina@gmail.com>
2020-12-03 17:17:43 -0700

    Add test for user story 2

--- spec/features/candy_shops/show_spec.rb
+++ spec/features/candy_shops/show_spec.rb
@@ -1,14 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe 'when I visit candy shops index page' do
-  it 'can see the name of each candy shop in the system' do
+RSpec.describe 'when I visit candy shops show page' do
+  it 'can see the candy shop and its attributes' do
 
   shop_1 = CandyShop.create!(name: "Sweet Tooth")
   shop_2 = CandyShop.create!(name: "Dentist's Friend")
 
-  visit '/candy_shops'
+  visit "/candy_shops/#{shop_1.id}"
 
   expect(page).to have_content(shop_1.name)
+
+  visit "/candy_shops/#{shop_2.id}"
+
   expect(page).to have_content(shop_2.name)
   end
 end
