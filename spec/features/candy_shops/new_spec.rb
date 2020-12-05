840f337a8066d0e225a4126996258397fb60b03f
Saundra <saundracatalina@gmail.com>
2020-12-05 15:49:47 -0700

    Refactor test for User story 3 to account for updated view using form_tag

--- spec/features/candy_shops/new_spec.rb
+++ spec/features/candy_shops/new_spec.rb
@@ -11,15 +11,14 @@ describe 'when I visit New Candy Shop page' do
   describe 'when I fill out the form with a new Candy Shop and click Create Candy Shop' do
     it 'redirects me to the Candy Shop Index page where I see the new Candy Shop displayed' do
       visit "/candy_shops/new"
- # save_and_open_page
-      fill_in 'Name', with: 'Lollipops R Us'
-      fill_in 'State', with: 'Maryland'
 
-      # click_on 'Create Candy Shop'
-      click_button 'Create Candy Shop'
+        fill_in "name", with: "Lollipops R Us"
+        fill_in "state", with: "Maryland"
 
-      expect(curent_path).to eq(/candy_shops)
-      expect(page).to have_content('Lollipops R Us')
+        click_on "Create Candy Shop"
+
+      expect(current_path).to eq('/candy_shops')
+      expect(page).to have_content("Lollipops R Us")
     end
   end
 end
