8febcab7f607c5f2efd87fda4f758d375e9b4226
Saundra <saundracatalina@gmail.com>
2020-12-04 20:21:12 -0700

    Refactor formatting

--- spec/features/candy_shops/show_spec.rb
+++ spec/features/candy_shops/show_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe 'when I visit candy shops show page' do
+RSpec.describe 'when I visit Candy Shops show page' do
   it 'can see the candy shop and its attributes' do
 
-  shop_1 = CandyShop.create!(name: "Sweet Tooth")
-  shop_2 = CandyShop.create!(name: "Dentist's Friend")
+    shop_1 = CandyShop.create!(name: "Sweet Tooth")
+    shop_2 = CandyShop.create!(name: "Dentist's Friend")
 
-  visit "/candy_shops/#{shop_1.id}"
+    visit "/candy_shops/#{shop_1.id}"
 
-  expect(page).to have_content(shop_1.name)
-  expect(page).to have_content(shop_1.state)
+    expect(page).to have_content(shop_1.name)
+    expect(page).to have_content(shop_1.state)
 
-  visit "/candy_shops/#{shop_2.id}"
+    visit "/candy_shops/#{shop_2.id}"
 
-  expect(page).to have_content(shop_2.name)
-  expect(page).to have_content(shop_2.state)
+    expect(page).to have_content(shop_2.name)
+    expect(page).to have_content(shop_2.state)
   end
 end
