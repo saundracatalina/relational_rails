45f7beec44c527c5fe3f5643f7591f0a6b077a7e
Saundra <saundracatalina@gmail.com>
2020-12-04 20:18:25 -0700

    Add test and link to new page

--- spec/features/candy_shops/index_spec.rb
+++ spec/features/candy_shops/index_spec.rb
@@ -1,14 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe 'when I visit candy shops index page' do
+RSpec.describe 'when I visit Candy Shops index page' do
   it 'can see the name of each candy shop in the system' do
 
-  shop_1 = CandyShop.create!(name: "Sweet Tooth")
-  shop_2 = CandyShop.create!(name: "Dentist's Friend")
+    shop_1 = CandyShop.create!(name: "Sweet Tooth")
+    shop_2 = CandyShop.create!(name: "Dentist's Friend")
 
-  visit '/candy_shops'
+    visit '/candy_shops'
 
-  expect(page).to have_content(shop_1.name)
-  expect(page).to have_content(shop_2.name)
+    expect(page).to have_content(shop_1.name)
+    expect(page).to have_content(shop_2.name)
+  end
+
+  it 'can see a link to create a new Candy Shop record' do
+
+    visit '/candy_shops'
+
+    expect(page).to have_link("New Candy Shop", href: '/candy_shops/new')
   end
 end
