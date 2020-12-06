04f550f0d5c24804e5c2bcf7370fac197e5d12cc
Saundra <saundracatalina@gmail.com>
2020-12-05 17:01:34 -0700

    Add tests for Update link

--- spec/features/candy_shops/show_spec.rb
+++ spec/features/candy_shops/show_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'when I visit Candy Shops show page' do
+describe 'when I visit Candy Shops show page' do
   it 'can see the candy shop and its attributes' do
 
     shop_1 = CandyShop.create!(name: "Sweet Tooth")
@@ -15,5 +15,24 @@ RSpec.describe 'when I visit Candy Shops show page' do
 
     expect(page).to have_content(shop_2.name)
     expect(page).to have_content(shop_2.state)
+  end
+
+  it 'has an Update Candy Shop link' do
+    shop_1 = CandyShop.create!(name: "Sweet Tooth")
+
+    visit "/candy_shops/#{shop_1.id}"
+
+    expect(page).to have_link("Update Candy Shop")
+  end
+describe 'when I click the Update Candy Shop link' do
+  it 'takes me to a page with a form to edit a Candy Shop' do
+    shop_1 = CandyShop.create!(name: "Sweet Tooth")
+
+    visit "/candy_shops/#{shop_1.id}"
+
+    click_on "Update Candy Shop"
+
+    expect(current_path).to eq("/candy_shops/#{shop_1.id}/edit")
+    end
   end
 end
