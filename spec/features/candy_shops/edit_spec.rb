b8020abdc4254e7224235b545abefba9e0747286
Saundra <saundracatalina@gmail.com>
2020-12-05 17:54:18 -0700

    Delete commented out notes

--- spec/features/candy_shops/edit_spec.rb
+++ spec/features/candy_shops/edit_spec.rb
@@ -16,10 +16,3 @@ describe 'When I fill out the form with updated information and click submit' do
     expect(page).to have_content("Kentucky")
   end
 end
-
-
-# When I fill out the form with updated information
-# And I click the button to submit the form
-# Then a `PATCH` request is sent to '/parents/:id',
-# the parent's info is updated,
-# and I am redirected to the Parent's Show page where I see the parent's updated info
