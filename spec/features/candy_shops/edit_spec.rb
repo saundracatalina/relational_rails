086dea64b8ce904291ab6779f1a53d7d410ba35f
Saundra <saundracatalina@gmail.com>
2020-12-05 17:02:53 -0700

    Add edit_spec to write tests

---
+++ spec/features/candy_shops/edit_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'When I fill out the form with updated information and click submit' do
+  it 'updates the Candy Shop information and shows the updated info' do
+    
+  end
+end
+
+
+# When I fill out the form with updated information
+# And I click the button to submit the form
+# Then a `PATCH` request is sent to '/parents/:id',
+# the parent's info is updated,
+# and I am redirected to the Parent's Show page where I see the parent's updated info
