c53d289bffc0d883a1c353761e36637fd0882a35
Saundra <saundracatalina@gmail.com>
2020-12-06 19:27:52 -0700

    Add show page tests

---
+++ spec/features/candies/show_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "when I visit a specific Candy's page" do
+  it "has the candy with that id and it's attributes" do
+    shop_1 = CandyShop.create!(name: "Sweet Tooth", state: "New York")
+    candy_1 = Candy.create!(name: "Taffy", quantity: 5, in_stock: true, candy_shop_id: "#{shop_1.id}")
+
+    visit "/candies/#{candy_1.id}"
+
+    expect(page).to have_content("Taffy")
+    expect(page).to have_content("5")
+    expect(page).to have_content("true")
+    expect(page).to have_content("#{candy_1.id}")
+  end
+end
