316b6a93f94e99bc417b158a5fc76e7b803de7c0
Saundra <saundracatalina@gmail.com>
2020-12-08 14:56:15 -0700

    Add test for Update Candy link

--- spec/features/candies/show_spec.rb
+++ spec/features/candies/show_spec.rb
@@ -12,4 +12,16 @@ describe "when I visit a specific Candy's page" do
     expect(page).to have_content("true")
     expect(page).to have_content("#{candy_1.id}")
   end
+  it "has a link to Update Candy and when clicked redirects to edit page" do
+    shop_1 = CandyShop.create!(name: "Sweet Tooth", state: "New York")
+    candy_1 = Candy.create!(name: "Taffy", quantity: 5, in_stock: true, candy_shop_id: "#{shop_1.id}")
+
+    visit "/candies/#{candy_1.id}"
+
+    expect(page).to have_link("Update Candy")
+
+    click_on "Update Candy"
+
+    expect(current_path).to eq("/candies/#{candy_1.id}/edit")
+  end
 end
