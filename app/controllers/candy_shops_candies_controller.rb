b461ae74ce723053068346f3d41b343872278461
Saundra <saundracatalina@gmail.com>
2020-12-07 20:09:19 -0700

    Add in_stock variable to support boolean response based on user input of quantity

--- app/controllers/candy_shops_candies_controller.rb
+++ app/controllers/candy_shops_candies_controller.rb
@@ -7,6 +7,18 @@ class CandyShopsCandiesController < ApplicationController
   end
 
   def new
-    
+
+  end
+
+  def create
+    in_stock = (params[:quantity].to_i > 0)
+
+    @candy = Candy.create!({name: params[:name],
+                  quantity: params[:quantity],
+                  in_stock: in_stock,
+                  candy_shop_id: params[:id]
+                  })
+
+    redirect_to "/candy_shops/#{@candy.candy_shop_id}/candies"
   end
 end
