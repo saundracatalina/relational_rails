b8ff391527ca9f6738c5b15f0edf0a0d74af69dd
Saundra <saundracatalina@gmail.com>
2020-12-08 19:22:40 -0700

    Add test to click Delete Candy button and get expected results

--- spec/features/candies/show_spec.rb
+++ spec/features/candies/show_spec.rb
@@ -24,12 +24,21 @@ describe "when I visit a specific Candy's show page" do
 
     expect(current_path).to eq("/candies/#{candy_1.id}/edit")
   end
-  it "has a link to Delete Candy and when clicked redirects to candy index and is no longer listed" do
+  it "has a button to Delete Candy and when clicked redirects to candy index and is no longer listed" do
     shop_1 = CandyShop.create!(name: "Sweet Tooth", state: "New York")
     candy_1 = Candy.create!(name: "Taffy", quantity: 5, in_stock: true, candy_shop_id: "#{shop_1.id}")
 
     visit "/candies/#{candy_1.id}"
 
     expect(page).to have_button("Delete Candy")
+
+    click_on "Delete Candy"
+
+    expect(current_path).to eq("/candies")
+
+    expect(page).to have_no_content("Taffy")
+    expect(page).to have_no_content("Quantity: 5")
+    expect(page).to have_no_content("In Stock: true")
+    expect(page).to have_no_content("Candy Shop Id: #{shop_1.id}")
   end
 end
