8553f1040e4cb1e3e8289a867e8e63d96ddffd23
Saundra <saundracatalina@gmail.com>
2020-12-09 19:22:14 -0700

    Add test for user input to return only candies over certain quantity

--- spec/features/candies/index_spec.rb
+++ spec/features/candies/index_spec.rb
@@ -21,26 +21,27 @@ describe 'When I visit the Candies index page' do
 
     expect(Candy.true_first.list_by_most_recent).to eq([candy_3, candy_1, candy_2])
   end
-  it "has a form with an input to Only return Candies with more than a specified number" do
-    shop_1 = CandyShop.create!(name: "Sweet Tooth", state: "New York")
-    candy_1 = Candy.create!(name: "Taffy", quantity: 7, in_stock: true, candy_shop_id: "#{shop_1.id}")
-    candy_2 = Candy.create!(name: "Fudge", quantity: 0, in_stock: false, candy_shop_id: "#{shop_1.id}")
-    candy_3 = Candy.create!(name: "Gobstoppers", quantity: 5, in_stock: true, candy_shop_id: "#{shop_1.id}")
-
-    visit '/candies'
-
-    expect(page).to have_content("Only return Candies with a quantity more than:")
-    expect(page).to have_field("Only return Candies with a quantity more than:")
-    expect(page).to have_button("Submit")
-
-    fill_in "Only return Candies with a quantity more than:", with: 6
-    click_on "Submit"
-
-    expect(current_path).to eq('/candies')
-    expect(page).to have_content("Taffy")
-    expect(page).to have_content("Quantity: 7")
-    expect(page).to have_content("In Stock: true")
-    expect(page).to have_content("Candy Shop Id: #{shop_1.id}")
-    expect(page).to have_content("Created On: #{candy_1.created_at.strftime("%m/%d/%y")}")
-  end
+  # it "has a form with an input to Only return Candies with more than a specified number" do
+  #   shop_1 = CandyShop.create!(name: "Sweet Tooth", state: "New York")
+  #   candy_1 = Candy.create!(name: "Taffy", quantity: 7, in_stock: true, candy_shop_id: "#{shop_1.id}")
+  #   candy_2 = Candy.create!(name: "Fudge", quantity: 0, in_stock: false, candy_shop_id: "#{shop_1.id}")
+  #   candy_3 = Candy.create!(name: "Gobstoppers", quantity: 5, in_stock: true, candy_shop_id: "#{shop_1.id}")
+  #
+  #   visit '/candies'
+  #
+  #   expect(page).to have_content("Only return Candies with a quantity more than:")
+  #   expect(page).to have_content("Type quantity here")
+  #   expect(page).to have_field("Type quantity here")
+  #   expect(page).to have_button("Submit")
+  #
+  #   fill_in "Only return Candies with a quantity more than:", with: 6
+  #   click_on "Submit"
+  #
+  #   expect(current_path).to eq('/candies')
+  #   expect(page).to have_content("Taffy")
+  #   expect(page).to have_content("Quantity: 7")
+  #   expect(page).to have_content("In Stock: true")
+  #   expect(page).to have_content("Candy Shop Id: #{shop_1.id}")
+  #   expect(page).to have_content("Created On: #{candy_1.created_at.strftime("%m/%d/%y")}")
+  # end
 end
